50d3fa5b3e32768a51c72135ee49692327f55567
Artur <arti996@yandex.ru>
2019-02-01 08:50:05 +0200

    added registration

--- backend/src/main/java/com/backend/backend/Repository/UserRepository.java
+++ backend/src/main/java/com/backend/backend/Repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.backend.backend.Repository;
 
 import com.backend.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
 }
--- backend/src/main/java/com/backend/backend/controllers/UserController.java
+++ backend/src/main/java/com/backend/backend/controllers/UserController.java
@@ -1,11 +1,19 @@
 package com.backend.backend.controllers;
 
+import com.backend.backend.Repository.UserRepository;
+import com.backend.backend.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/api/user")
 @CrossOrigin(origins = "*")
 public class UserController {
+    @Autowired
+    private UserRepository userRepo;
 
     // Login in
     @GetMapping("/login")
@@ -15,8 +23,18 @@ public class UserController {
 
     // User create
     @PostMapping("/user/create")
-    public boolean userCreate() {
-        return true;
+    public String userCreate(User user, Map<String, Object> model) {
+        User userFromDb = userRepo.findByEmail(user.getEmail());
+
+        if (userFromDb != null){
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+        userRepo.save(user);
+
+        return "redirect:/login";
+
     }
 
     // Delete user
