82c2e79042b4dde7d8fc9f27eb82dd67b487e478
art <ArturAzizean>
2019-05-13 09:50:17 +0300

    added security (for token)

---
+++ backend/src/main/java/com/backend/backend/security/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.backend.backend.security.config;
+
+import com.backend.backend.security.filters.TokenAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+/**
+ * 02.08.2017
+ *Филтер преобразует токен в объект аутентификации и скармливает security.
+ *Security сам вызывает провайдер, в котором у нас находится токен и проверка на валидность
+ */
+@ComponentScan("com.backend.backend")
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    //
+
+    @Autowired
+    private TokenAuthFilter tokenAuthFilter;
+
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .addFilterBefore(tokenAuthFilter, BasicAuthenticationFilter.class)
+                .antMatcher("/**")
+                .authenticationProvider(authenticationProvider)
+                .authorizeRequests()
+                .antMatchers("/users/**").hasAuthority("USER")
+                .antMatchers("/login").permitAll();
+        http.csrf().disable();
+    }
+}
