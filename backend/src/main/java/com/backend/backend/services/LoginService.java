ee74e04b9c40d22d44c52cb0314d8eb9ee4cc4f0
artur.a <arti996@yandex.ru>
2019-08-28 16:47:43 +0300

    rename backend directory

--- backend/src/main/java/com/backend/backend/services/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.backend.backend.services;
-
-
-import com.backend.backend.forms.LoginForm;
-import com.backend.backend.transfer.TokenDto;
-
-public interface LoginService {
-    TokenDto login(LoginForm loginForm);
-}
